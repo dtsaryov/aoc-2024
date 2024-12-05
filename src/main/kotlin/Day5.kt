7171f899af9cd3198cd3ab02ad15347e357dca5e
Daniil Tsarev <daniil.tsaryov@jetbrains.com>
2024-12-05 23:03:31 +0100

    Optimize Day 5 a bit

--- src/main/kotlin/Day5.kt
+++ src/main/kotlin/Day5.kt
@@ -1,14 +1,14 @@
-private val RULE_REGEX = Regex("(\\d+)\\|(\\d+)")
+private val RULE_REGEX = Regex("\\d+\\|\\d+")
 
 object Day5 {
 
     fun part1(inputFileName: String): Int {
-        val (before, after, updates) = getRulesAndUpdates(inputFileName)
+        val (before, updates) = getRulesAndUpdates(inputFileName)
         var result = 0
         for (line in updates) {
             var correct = true
             for (i in line.indices)
-                if (!checkBefore(i, line, after) || !checkAfter(i, line, before)) {
+                if (!checkAfter(i, line, before)) {
                     correct = false
                     break
                 }
@@ -20,14 +20,14 @@ object Day5 {
     }
 
     fun part2(inputFileName: String): Int {
-        val (valuesBeforeKey, valuesAfterKey, updates) = getRulesAndUpdates(inputFileName)
+        val (valuesBeforeKey, updates) = getRulesAndUpdates(inputFileName)
 
         var result = 0
 
         for (line in updates) {
             var correct = true
             for (i in line.indices) {
-                if (!checkBefore(i, line, valuesAfterKey) || !checkAfter(i, line, valuesBeforeKey)) {
+                if (!checkAfter(i, line, valuesBeforeKey)) {
                     correct = false
                     break
                 }
@@ -46,19 +46,17 @@ object Day5 {
         return result
     }
 
-    private fun getRulesAndUpdates(inputFileName: String): Triple<MutableMap<Int, Set<Int>>, MutableMap<Int, Set<Int>>, MutableList<List<Int>>> {
-        val before = mutableMapOf<Int, Set<Int>>() // values – numbers before a key
-        val after = mutableMapOf<Int, Set<Int>>() // values - numbers after a key
+    private fun getRulesAndUpdates(inputFileName: String): Pair<MutableMap<Int, Set<Int>>, MutableList<List<Int>>> {
+        val rules = mutableMapOf<Int, Set<Int>>() // values – numbers before a key
 
         val updates = mutableListOf<List<Int>>()
 
         for (line in getInputLines(inputFileName)) {
             if (RULE_REGEX.matches(line)) {
-                val (_, s1, s2) = RULE_REGEX.find(line)?.groupValues ?: continue
+                val (s1, s2) = line.split('|')
                 val (n1, n2) = s1.toInt() to s2.toInt() // before, after
 
-                before[n2] = before.getOrDefault(n2, emptySet()) + n1
-                after[n1] = after.getOrDefault(n1, emptySet()) + n2
+                rules[n2] = rules.getOrDefault(n2, emptySet()) + n1
             } else if (line.isEmpty()) {
                 continue
             } else {
@@ -66,18 +64,7 @@ object Day5 {
             }
         }
 
-        return Triple(before, after, updates)
-    }
-
-    // check no numbers before that should be after
-    private fun checkBefore(i: Int, line: List<Int>, after: Map<Int, Set<Int>>): Boolean {
-        val n = line[i]
-        for (j in 0 until i) {
-            val rulesAfter = after[n] ?: emptySet()
-            if (line[j] in rulesAfter)
-                return false
-        }
-        return true
+        return rules to updates
     }
 
     // check no numbers before that should be after
