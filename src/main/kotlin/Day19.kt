231d5da47198b825b2596448f47c24a15c5d6ce5
Daniil Tsarev <daniil.tsaryov@jetbrains.com>
2024-12-19 21:16:16 +0100

    Day 19 part 2

--- src/main/kotlin/Day19.kt
+++ src/main/kotlin/Day19.kt
@@ -7,18 +7,52 @@ object Day19 {
         val targets = input.drop(2)
 
         return targets.sumOf { s ->
-            if (findNextPattern(s, patterns)) 1L
+            if (hasMatchingPatternSet(s, patterns)) 1L
             else 0L
         }
     }
 
-    private fun findNextPattern(s: String, patterns: List<String>): Boolean {
+    private fun hasMatchingPatternSet(s: String, patterns: List<String>): Boolean {
         return patterns.firstOrNull { p ->
             when {
                 s.isEmpty() -> true
-                s.startsWith(p) -> findNextPattern(s.substringAfter(p), patterns)
+                s.startsWith(p) -> hasMatchingPatternSet(s.substringAfter(p), patterns)
                 else -> false
             }
         } != null
+    }
+
+    fun part2(inputFileName: String): Long {
+        val input = getInputLines(inputFileName)
+
+        val patterns = input[0].split(',').map { it.trim() }
+        val targets = input.drop(2)
+
+        val cache = mutableMapOf<String, Long>()
+
+        return targets.sumOf { s ->
+            countVariants(s, patterns, cache)
+        }
+    }
+
+    private fun countVariants(
+        s: String,
+        patterns: List<String>,
+        cache: MutableMap<String, Long>
+    ): Long {
+        var result = cache[s]
+        if (result != null) return result
+
+        result = patterns.sumOf { p ->
+            when {
+                s == p -> 1
+                s.startsWith(p) -> countVariants(s.substringAfter(p), patterns, cache)
+                else -> 0
+            }
+        }
+
+        cache[s] = result
+
+        return result
     }
 }
--- src/test/kotlin/Day19Test.kt
+++ src/test/kotlin/Day19Test.kt
@@ -12,4 +12,14 @@ class Day19Test {
     fun testPart1() {
         assertEquals(342, Day19.part1("day19_input.txt"))
     }
+
+    @Test
+    fun testPart2Example1() {
+        assertEquals(16, Day19.part2("day19_test_input.txt"))
+    }
+
+    @Test
+    fun testPart2() {
+        assertEquals(891192814474630, Day19.part2("day19_input.txt"))
+    }
 }
